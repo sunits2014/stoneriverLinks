36dfa244c581e6586849d505ec35490ac7915a57
vipulpathe <vipulpathe@gmail.com>
2017-11-22 18:06:25 +0530

    Code formatting
    Replaced double quotes with single quotes to maintain consistency

--- src/app/route/route.module.ts
+++ src/app/route/route.module.ts
@@ -8,17 +8,17 @@ import { HomeComponent } from '../home/home.component';
 import { AddRegionComponent } from '../add-region/add-region.component';
 
 const routes: Routes = [
-  { path: "", pathMatch: "full", redirectTo: "stoneriver" },
+  { path: '', pathMatch: 'full', redirectTo: 'stoneriver' },
   {
-    path: "stoneriver",
+    path: 'stoneriver',
     component: LinksComponent, children: [
-      { path: "", redirectTo: "home", pathMatch: 'full' },
-      { path: "home", component: HomeComponent, data: {title: "Home"} },      
-      { path: "lifeportrait", component: LifeportraitComponent, data: {title: "Life Portrait"} },
-      { path: "lifesuite", component: LifesuiteComponent, data: {title: "Life Suite"} },
-      { path: "addData", component: AddRegionComponent, data: {title: "Add Region Data"} },
-      { path: "updateData", component: AddRegionComponent, data: {title: "Update Region Data"} },
-      { path: "deleteData", component: AddRegionComponent, data: {title: "Delete Region Data"} }
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent, data: {title: 'Home'} },
+      { path: 'lifeportrait', component: LifeportraitComponent, data: {title: 'Life Portrait'} },
+      { path: 'lifesuite', component: LifesuiteComponent, data: {title: 'Life Suite'} },
+      { path: 'addData', component: AddRegionComponent, data: {title: 'Add Region Data'} },
+      { path: 'updateData', component: AddRegionComponent, data: {title: 'Update Region Data'} },
+      { path: 'deleteData', component: AddRegionComponent, data: {title: 'Delete Region Data'} }
     ]
   }
 
